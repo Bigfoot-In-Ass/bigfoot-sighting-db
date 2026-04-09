06c550428502b63bd47ec12b32fb93e5da4807a5
Dan Johnson <jdjohnson8883@gmail.com>
2026-04-09 18:20:15 -0400

    Rough draft of table creation script

--- phase-3.sql
+++ phase-3.sql
@@ -36,4 +36,45 @@ CREATE TABLE FOLLOW_UP (
     PRIMARY KEY (Report_ID, Investigator_ID, Follow_Up_Date),
     FOREIGN KEY (Report_ID) REFERENCES REPORT(Report_ID) ON DELETE CASCADE,
     FOREIGN KEY (Investigator_ID) REFERENCES INVESTIGATOR(Investigator_ID) ON DELETE CASCADE
-)
+);
+
+CREATE TABLE ENVIRONMENT (
+    Environment_ID SERIAL PRIMARY KEY,
+    -- may need to process this into a concatenated string
+    Environment_Description TEXT,
+    Environment_Details TEXT,
+    Season VARCHAR(20),
+    -- May need to group date and time information into a single column
+    -- Date DATE,
+    -- process each date using the information in the report
+    Year INT CHECK (Year <= EXTRACT(YEAR FROM CURRENT_DATE)),
+    Month INT CHECK (Month >= 1 AND Month <= 12),
+    Date_Text VARCHAR(20),
+    Time_And_Conditions VARCHAR(100)
+);
+
+CREATE TABLE LOCATION (
+    Location_ID SERIAL PRIMARY KEY,
+    State VARCHAR(50),
+    County VARCHAR(50),
+    Nearest_Town VARCHAR(50),
+    Nearest_Road VARCHAR(50),
+    Location_Details TEXT,
+    A_G_Reference VARCHAR(50)
+);
+
+CREATE TABLE EVENT (
+    Event_ID SERIAL PRIMARY KEY,
+    Event_Type VARCHAR(50) NOT NULL,
+    -- may need to process this into a concatenated string
+    Observerd_Description TEXT,
+    Other_Witnesses TEXT,
+    Witness_other_stories TEXT,
+    Also_Noticed TEXT,
+    Report_ID INT NOT NULL,
+    Environment_ID INT,
+    Location_ID INT,
+    FOREIGN KEY (Report_ID) REFERENCES REPORT(Report_ID) ON DELETE CASCADE,
+    FOREIGN KEY (Environment_ID) REFERENCES ENVIRONMENT(Environment_ID) ON DELETE SET NULL,
+    FOREIGN KEY (Location_ID) REFERENCES LOCATION(Location_ID) ON DELETE SET NULL
+);
