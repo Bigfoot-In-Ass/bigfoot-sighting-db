1b292544774b5f88e75da7ca5b9e873b628d74ed
bmeudy11 <bmeudy@gmail.com>
2026-04-21 19:22:31 -0400

    add everyone's queries for phase 5

--- phase-5.sql
+++ phase-5.sql
@@ -21,3 +21,73 @@ FROM sightings.report r
               ON r.source_id = s.source_id
 GROUP BY s.name
 ORDER BY total_reports DESC;
+
+
+--3. 3.	Who has performed the least follow-up investigations? Who has
+-- performed the most follow-up investigations?
+
+WITH inv_follow_ups AS (SELECT
+                            inv.investigator_id,
+                            inv.investigator_name ||
+                            CASE
+                                WHEN inv.title IS NOT NULL THEN ', ' || inv.title
+                                ELSE ''
+                                END  ||
+                            CASE
+                                WHEN inv.organization IS NOT NULL THEN ' (' || inv.organization || ')'
+                                ELSE ''
+                                END AS investigator,
+                            COUNT(fu.*) AS follow_up_count
+                        FROM
+                            sightings.investigator inv
+                                JOIN
+                            sightings.follow_up fu
+                            ON
+                                inv.investigator_id = fu.investigator_id
+                        GROUP BY
+                            inv.investigator_id)
+SELECT
+    'Investigator with the most follow-ups' AS Record,
+    investigator AS Investigator,
+    follow_up_count AS "Number of Follow-ups"
+FROM
+    inv_follow_ups
+WHERE investigator_id =
+      (SELECT investigator_id
+       FROM inv_follow_ups
+       WHERE follow_up_count = (SELECT MAX(follow_up_count) FROM inv_follow_ups)
+       LIMIT 1)
+UNION ALL
+SELECT
+    'Investigator with the least follow-ups' AS Record,
+    investigator AS Investigator,
+    follow_up_count AS "Number of Follow-ups"
+FROM
+    inv_follow_ups
+WHERE investigator_id =
+      (SELECT investigator_id
+       FROM inv_follow_ups
+       WHERE follow_up_count = (SELECT MIN(follow_up_count) FROM inv_follow_ups) LIMIT 1);
+
+--4. Are there documented simultaneous Bigfoot sightings?
+SELECT
+    env.year,
+    env.month,
+    env.date_text,
+    COUNT(*) AS sightings_count
+FROM sightings.event e
+         JOIN sightings.environment env
+              ON env.environment_id = e.environment_id
+WHERE env.year IS NOT NULL
+  AND env.month IS NOT NULL
+  AND env.date_text IS NOT NULL
+GROUP BY env.year, env.month, env.date_text
+HAVING COUNT(*) > 1
+ORDER BY env.year, env.month, env.date_text;
+
+
+
+--5. How many reports do not have follow-up investigations?
+SELECT
+    (SELECT COUNT(*) FROM sightings.follow_up) AS num_follow_ups,
+    (SELECT COUNT(*) FROM sightings.event) AS num_events;
